0b8d62e4a933988bab79e3151641baad5682b5cf
SetazeR <we.are@setazer.us>
2023-10-23 12:44:06 +0500

    fixes

--- build.py
+++ build.py
@@ -1,33 +1,38 @@
+
 import json
+import os
 import pathlib
 import shutil
 import zipfile
 
 
+factorio_path = pathlib.Path(os.getenv('APPDATA')) / 'Factorio' / 'mods'
+
 # Folder shenanigans and getting actual name
-name = 'factorio_2ish'
+name = 'factorio-2ish'
 src = pathlib.Path(name)
 with (src / 'info.json').open('r') as f:
     info = json.load(f)
 version = info['version']
 target_name = f'{name}_{version}'
-out_dir = pathlib.Path('out')
-build_dir = out_dir / target_name
+out_path = pathlib.Path('out')
+build_path = out_path / target_name
 
 # prepare files for zip
-shutil.copytree(src, build_dir, dirs_exist_ok=True)
+shutil.copytree(src, build_path, dirs_exist_ok=True)
 
 # create zip
-zip_name = out_dir / f'{target_name}.zip'
+zip_name = out_path / f'{target_name}.zip'
 with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
-    for item in build_dir.rglob('*'):
+    for item in build_path.rglob('*'):
         if not item.is_file():
             continue
-        rel_path = item.relative_to(out_dir)
+        rel_path = item.relative_to(out_path)
         zipf.write(item, rel_path)
 
 # clean build dir
-shutil.rmtree(build_dir, ignore_errors=True)
+shutil.rmtree(build_path, ignore_errors=True)
 
 print('Build complete:', zip_name.absolute().as_uri())
+shutil.copyfile(zip_name, factorio_path / zip_name.name)
 
