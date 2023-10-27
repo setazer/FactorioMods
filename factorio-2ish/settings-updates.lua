a9add2536e7e106212c6f1e3904ced443b3696d2
SetazeR <we.are@setazer.us>
2023-10-27 16:46:35 +0500

    + AutoTrash * Changed numeration + some file redistribution

--- factorio-2ish/settings-updates.lua
+++ factorio-2ish/settings-updates.lua
@@ -4,16 +4,7 @@
 --change_default("tg-tile-ghost-tint", {r = 0.3, g = 0.6, b = 0.6, a = 0.3})
 
 if mods.TintedGhosts then
-    if not data.raw["color-setting"]["tg-tile-ghost-tint"] then
-        data:extend({
-          {
-            type = "color-setting",
-            name = "tg-tile-ghost-tint",
-            setting_type = "startup",
-            default_value = {r = 0.2, g = 0.36, b = 0.78, a = 0.4},
-          },
-        })
-    end
     data.raw["color-setting"]["tg-ghost-tint"].default_value = {r = 0.2, g = 0.36, b = 0.78, a = 0.3}
+    data.raw["color-setting"]["tg-tile-ghost-tint"].default_value = {r = 0.2, g = 0.36, b = 0.78, a = 0.4}
 end
 
