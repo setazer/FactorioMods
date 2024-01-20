81a54d2fe68c0dc8355d8429f75b506826f6477c
SetazeR <we.are@setazer.us>
2024-01-20 21:36:33 +0500

    Fixed migration conflict swapping blacklist/whitelist modes, fixed event entities in control.lua

--- FilterInsertersBegone/control.lua
+++ FilterInsertersBegone/control.lua
@@ -1,10 +1,13 @@
 function OnBuiltInserter(event)
-   local entity = event.created_entity
+   local entity = event.created_entity or event.entity or event.destination
    entity.inserter_filter_mode = "blacklist"
 end
 
+--created_entity
 script.on_event(defines.events.on_built_entity, OnBuiltInserter,  {{filter="type", type="inserter"}})
-script.on_event(defines.events.on_entity_cloned, OnBuiltInserter,  {{filter="type", type="inserter"}})
 script.on_event(defines.events.on_robot_built_entity, OnBuiltInserter,  {{filter="type", type="inserter"}})
+--destination
+script.on_event(defines.events.on_entity_cloned, OnBuiltInserter,  {{filter="type", type="inserter"}})
+--entity
 script.on_event(defines.events.script_raised_built, OnBuiltInserter,  {{filter="type", type="inserter"}})
 script.on_event(defines.events.script_raised_revive, OnBuiltInserter,  {{filter="type", type="inserter"}})
--- FilterInsertersBegone/migrations/FilterInsertersBegone_0.1.3.lua
+++ FilterInsertersBegone/migrations/FilterInsertersBegone_0.1.3.lua
@@ -1,10 +1,13 @@
 local i = 0
 for _, surface in pairs(game.surfaces) do
-    for _, inserter in pairs(surface.find_entities_filtered{type='inserter', name~={'filter-inserter', 'stack-filter-inserter'}}) do
-        inserter.inserter_filter_mode = 'blacklist'
+    for _, inserter in pairs(surface.find_entities_filtered{type='inserter'}) do
+        if inserter.name ~= 'filter-inserter' and inserter.name ~= 'stack-filter-inserter' then
+            inserter.inserter_filter_mode = 'blacklist'
+        end
     end
 
     for _, inserter in pairs(surface.find_entities_filtered{type='inserter', name={'filter-inserter', 'stack-filter-inserter'}}) do
+        inserter_mode = inserter.inserter_filter_mode
         local replacement = surface.create_entity{
             name = inserter.name == 'filter-inserter' and 'fast-inserter' or 'stack-inserter',
             position = inserter.position,
@@ -18,6 +21,7 @@ for _, surface in pairs(game.surfaces) do
                 replacement.connect_neighbour(wire)
             end
             replacement.copy_settings(inserter)
+            replacement.inserter_filter_mode = inserter_mode
             inserter.destroy()
             i = i + 1
         else
